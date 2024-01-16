ebb9b42d8a3c5158d5b4fc80278becf4a1752b8a
pankaj <pankajkalra09@gmail.com>
2024-01-16 10:24:48 -0500

    parker

--- src/main/java/AdmPages/DashboardClass.java
+++ src/main/java/AdmPages/DashboardClass.java
@@ -45,7 +45,7 @@ public class DashboardClass {
 		this.driver = driver;
 	}
 
-	//philip
+	//parker
 	public void verifyPageTitle() {
 		String pageTitle = driver.getTitle();
 		log.info("Page title is " + pageTitle);
